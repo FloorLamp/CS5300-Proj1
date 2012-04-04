9349fd5c9bd9c357fb42efdf9b59121c85aa9308
Wenceslaus Lee <wwl45@cornell.edu>
2012-04-03 20:04:36 -0400

    Set up template

--- P1/src/session/Session.java
+++ P1/src/session/Session.java
@@ -2,54 +2,18 @@ package session;
 
 import groupMembership.Server;
 
-import java.util.Date;
-
 public class Session {
-	private Server creator;
-	private Integer sess_num;
-	private Integer change_count;
-	private Server primary,backup;
-	private Date timestamp;
+	private Integer changecount;
 	private String message;
+	private Long expiration;
+	private Server primary,backup;
 	
-	public Session(Server creator,Integer sess_num,
-			Server primary, Server backup) {
-		this.setCreator(creator);
-		this.setSess_num(sess_num);
-		this.setPrimary(primary);
-		this.setBackup(backup);
-		this.change_count=0;
-		this.timestamp = new Date();
-	}
-
-	public void updateTimestamp() {
-		this.timestamp = new Date();
-	}
-	//Gets timestamp of session in seconds since epoch
-	public long getTimestamp() {
-		return timestamp.getTime()/1000;
-	}
-
-	public void incrementVersion() {
-		this.change_count++;
-	}
-
-	public Server getCreator() {
-		return creator;
-	}
-
-	public void setCreator(Server creator) {
-		this.creator = creator;
+	Session(Integer a, String b, Long c) {
+		this.setChangecount(a);
+		this.setMessage(b);
+		this.setExpiration(c);
 	}
 	
-	public Integer getSess_num() {
-		return sess_num;
-	}
-
-	public void setSess_num(Integer sess_num) {
-		this.sess_num = sess_num;
-	}
-
 	public Server getPrimary() {
 		return primary;
 	}
@@ -57,13 +21,21 @@ public class Session {
 	public void setPrimary(Server primary) {
 		this.primary = primary;
 	}
-
+	
 	public Server getBackup() {
 		return backup;
 	}
 
 	public void setBackup(Server backup) {
 		this.backup = backup;
+	}
+
+	public Integer getChangecount() {
+		return changecount;
+	}
+
+	public void setChangecount(Integer changecount) {
+		this.changecount = changecount;
 	}
 
 	public String getMessage() {
@@ -73,10 +45,13 @@ public class Session {
 	public void setMessage(String message) {
 		this.message = message;
 	}
-	
-	public String toString() {
-		return sess_num+","+creator.toString()+","+","+
-				change_count+","+primary.toString()+","+
-				backup.toString();
+
+	public Long getExpiration() {
+		return expiration;
 	}
+
+	public void setExpiration(Long expiration) {
+		this.expiration = expiration;
+	}
+	
 }
