c30d45298b1a843f2d56b45bceb1db3bad506c02
Jasdeep Hundal <dsjas297@gmail.com>
2012-04-06 11:55:00 -0400

    basic cache finished

--- P1/src/session/SessionCache.java
+++ P1/src/session/SessionCache.java
@@ -12,18 +12,36 @@ public class SessionCache {
 	protected static final Lock readlock = rwl.readLock();
 	protected static final Lock writelock = rwl.writeLock();
 	
-	protected static final Hashtable<String, Session> cached_sessions = new Hashtable<String, Session>();
+	protected static final Hashtable<String, Session> cache = new Hashtable<String, Session>();
 	protected static final LinkedList<String> session_order = new LinkedList<String>();
 	
 	public SessionCache(){
 		
-	};
+	}
 	
-	public Session readFromCache(){
-		
+	public Session readFromCache(String string){
+		readlock.lock();
+		Session retval = cache.get(string);
+		readlock.unlock();
+		return retval;
 	}
 	
 	public void addToCache(String string, Session session){
+		writelock.lock();
+
+		if (readFromCache(string) != null){
+			session_order.remove(string);
+		}
 		
+		// If it's not in the cache and the cache is full
+		else if(session_order.size() > CACHE_SIZE){
+			String str = session_order.pop();
+			cache.remove(str);
+		}
+		
+		cache.put(string, session);
+		session_order.add(string);
+		
+		writelock.unlock();
 	}
 }
