3146d2b5a188f2a40550df1b6b41a096e22ed3cf
Jasdeep Hundal <dsjas297@gmail.com>
2012-04-06 01:06:52 -0400

    Adding caching 2

--- P1/src/session/SessionCache.java
+++ P1/src/session/SessionCache.java
@@ -1,5 +1,7 @@
 package session;
 
+import java.util.Hashtable;
+import java.util.LinkedList;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
 
@@ -10,5 +12,18 @@ public class SessionCache {
 	protected static final Lock readlock = rwl.readLock();
 	protected static final Lock writelock = rwl.writeLock();
 	
+	protected static final Hashtable<String, Session> cached_sessions = new Hashtable<String, Session>();
+	protected static final LinkedList<String> session_order = new LinkedList<String>();
 	
+	public SessionCache(){
+		
+	};
+	
+	public Session readFromCache(){
+		
+	}
+	
+	public void addToCache(String string, Session session){
+		
+	}
 }
